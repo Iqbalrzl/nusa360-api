3ab0d8c0a0bc09577d458442af5324ec8f206d10
Iqbaal <iqbaalrizaaldy1@gmail.com>
2025-08-05 21:00:03 +0700

    Initiate Project

---
+++ src/main/java/com/troopers/nusa360/Nusa360Application.java
@@ -0,0 +1,13 @@
+package com.troopers.nusa360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nusa360Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nusa360Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/troopers/nusa360/Nusa360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troopers.nusa360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nusa360ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
